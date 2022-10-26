8cb6b1b270ba4683a0e3a5cce6a5e678f100a886
DJ Kehoe <donald.j.kehoe@gmail.com>
2022-10-26 13:38:19 -0400

    small fix for draw

--- src/game.c
+++ src/game.c
@@ -16,7 +16,6 @@
 #include "gf2d_sprite.h"
 #include "gf2d_font.h"
 #include "gf2d_draw.h"
-#include "gf2d_actor.h"
 
 #include "entity.h"
 #include "agumon.h"
@@ -54,8 +53,7 @@ int main(int argc,char *argv[])
     gf3d_vgraphics_init("config/setup.cfg");
     gf2d_font_init("config/font.cfg");
     gf2d_draw_manager_init(1000);
-    gf2d_actor_init(1024);
-
+    
     slog_sync();
     
     entity_system_init(1024);
@@ -112,10 +110,10 @@ int main(int argc,char *argv[])
                     gf3d_particle_draw(&particle[a]);
                 }
             //2D draws
-                gf2d_draw_rect_filled(gfc_rect(6,6,1000,40),gfc_color(0.5,0.5,0.5,1));
+                gf2d_draw_rect_filled(gfc_rect(10 ,10,1000,32),gfc_color8(128,128,128,255));
                 gf2d_font_draw_line_tag("Press ALT+F4 to exit",FT_H1,gfc_color(1,1,1,1), vector2d(10,10));
                 
-                gf2d_draw_rect(gfc_rect(6,6,1000,40),gfc_color(1,1,0,1));
+                gf2d_draw_rect(gfc_rect(10 ,10,1000,32),gfc_color8(255,255,255,255));
                 
                 gf2d_sprite_draw(mouse,vector2d(mousex,mousey),vector2d(2,2),vector3d(8,8,0),gfc_color(0.3,.9,1,0.9),(Uint32)mouseFrame);
         gf3d_vgraphics_render_end();
