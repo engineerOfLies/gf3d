0779f809e976e8846d3cad28391342fbaa6f3992
DJ Kehoe <donald.j.kehoe@gmail.com>
2023-02-05 13:58:12 -0500

    images and shaders now load through pak file

--- src/gf3d_shaders.c
+++ src/gf3d_shaders.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "simple_logger.h"
+
+#include "gfc_pak.h"
 
 #include "gf3d_shaders.h"
 
@@ -24,32 +27,14 @@ VkShaderModule gf3d_shaders_create_module(const char *shader,size_t size,VkDevic
 char *gf3d_shaders_load_data(const char * filename,size_t *rsize)
 {
     char *buffer = NULL;
-    FILE *file;
     size_t size;
-    file = fopen(filename,"rb");
-    if (!file)
-    {
-        slog("failed to open shader file %s",filename);
-        return NULL;
-    }
-    fseek(file,0,SEEK_END);
-    size = ftell(file);
-    if (!size)
-    {
-        slog("file %s contained no data",filename);
-        fclose(file);
-        return NULL;
-    }
-    rewind(file);
-    buffer = gfc_allocate_array(sizeof(char),size);
+    
+    buffer = gfc_pak_file_extract(filename,&size);
     if (!buffer)
     {
-        slog("failed to allocate memory for shader file %s",filename);
-        fclose(file);
+        slog("failed to laod shader file %s",filename);
         return NULL;
-    }
-    fread(buffer,size,1,file);
-    fclose(file);
+    }    
     if (rsize)*rsize = size;
     return buffer;
 }
